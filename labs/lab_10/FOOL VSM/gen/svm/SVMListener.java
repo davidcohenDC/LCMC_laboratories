fe3a41edf0b29b69d5e0847b88d308838065cec2
david <david.cohen@studio.unibo.it>
2026-06-11 11:15:50 +0200

    chore: move laboratories in labs folder

---
+++ labs/lab_10/FOOL VSM/gen/svm/SVMListener.java
@@ -0,0 +1,31 @@
+// Generated from C:/Users/David/Github/university/first_year/first_semester/lcmc/LCMC_laboratories/lab_10/FOOL VSM/src/svm/SVM.g4 by ANTLR 4.13.2
+package svm;
+
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link SVMParser}.
+ */
+public interface SVMListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link SVMParser#assembly}.
+	 * @param ctx the parse tree
+	 */
+	void enterAssembly(SVMParser.AssemblyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SVMParser#assembly}.
+	 * @param ctx the parse tree
+	 */
+	void exitAssembly(SVMParser.AssemblyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SVMParser#instruction}.
+	 * @param ctx the parse tree
+	 */
+	void enterInstruction(SVMParser.InstructionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SVMParser#instruction}.
+	 * @param ctx the parse tree
+	 */
+	void exitInstruction(SVMParser.InstructionContext ctx);
+}
---
+++ labs/lab_10/FOOL VSM/gen/svm/SVMVisitor.java
@@ -0,0 +1,26 @@
+// Generated from C:/Users/David/Github/university/first_year/first_semester/lcmc/LCMC_laboratories/lab_10/FOOL VSM/src/svm/SVM.g4 by ANTLR 4.13.2
+package svm;
+
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link SVMParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface SVMVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link SVMParser#assembly}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAssembly(SVMParser.AssemblyContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link SVMParser#instruction}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitInstruction(SVMParser.InstructionContext ctx);
+}
